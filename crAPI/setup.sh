90e9a992cc97ab9b10be109885c067bccae19a5c
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-01-26 01:59:29 +0000

    Add setup script for crAPI.

---
+++ crAPI/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -o docker-compose.yml https://raw.githubusercontent.com/OWASP/crAPI/main/deploy/docker/docker-compose.yml
+docker-compose pull
+#docker-compose -f docker-compose.yml --compatibility up -d
