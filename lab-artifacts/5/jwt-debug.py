1e8e7acfbf9cd476e8f8a8e1937ba383512dcafb
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-02-17 23:55:17 +0000

    Print human-readable expiration for debugging.

--- lab-artifacts/5/jwt-debug.py
+++ lab-artifacts/5/jwt-debug.py
@@ -1,6 +1,11 @@
 import sys
 import base64
 import json
+from datetime import datetime
+
+# Credit to https://stackoverflow.com/a/3682808.
+def format_timestamp(ts:int):
+    return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 assert len(sys.argv) == 2
 jwt = sys.argv[1]
@@ -18,3 +23,7 @@ print(json.dumps(header, indent=4))
 # Also, see https://gist.github.com/perrygeo/ee7c65bb1541ff6ac770.
 payload = json.loads(base64.urlsafe_b64decode(tokens[1] + '==')) # Kludge for incorrect padding.
 print(json.dumps(payload, indent=4))
+
+if 'exp' in payload:
+    exp = format_timestamp(int(payload['exp']))
+    print(f"Expires at {exp}.")
