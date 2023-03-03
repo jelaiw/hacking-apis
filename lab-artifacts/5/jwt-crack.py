76c2999776112f481c773230a8771a452b50275e
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-03-02 19:51:40 -0600

    Add code comments clarifying byte encoding.

--- lab-artifacts/5/jwt-crack.py
+++ lab-artifacts/5/jwt-crack.py
@@ -13,6 +13,8 @@ assert len(words) == 3
 print(words[2]) # Print tag for debugging.
 
 data = words[0] + DELIMITER + words[1]
+# See https://stackoverflow.com/a/43882903.
+# Note comment at https://stackoverflow.com/questions/31848293/python3-and-hmac-how-to-handle-string-not-being-binary#comment125847252_43882903.
 data = data.encode('ascii')
 
 key = b'crapi'
