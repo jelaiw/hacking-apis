3ffd5d9d839fabeba7b9f069fbe9bfee6e759c71
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-04-11 23:19:37 +0000

    Link to GCP provider docs for compute_instance.

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,7 @@ resource "google_compute_network" "hapinet" {
   name = "hapinet"
 }
 
+# See https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance.
 resource "google_compute_instance" "kali" {
   name         = "kali"
   machine_type = "f1-micro"
