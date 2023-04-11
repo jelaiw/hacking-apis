d3ca910ea556c30da8ebb89f7037cf2eb21f8b89
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-04-11 14:41:27 +0000

    Update with VM boilerplate for Kali.

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,20 @@ provider "google" {
 resource "google_compute_network" "hapinet" {
   name = "hapinet"
 }
+
+resource "google_compute_instance" "kali" {
+  name         = "kali"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.hapinet.name
+    access_config {
+    }
+  }
+}
