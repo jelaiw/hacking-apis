103791711f84885a5f398e239ebbbac49d6d49de
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-05-03 02:01:26 +0000

    Move Kali VM TF config to kali.tf.

---
+++ terraform/kali.tf
@@ -0,0 +1,17 @@
+# See https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance.
+resource "google_compute_instance" "kali" {
+  name         = "kali"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "kali-linux-2023-1-cloud-genericcloud-amd64"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.hapinet.name
+    access_config {
+    }
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -16,21 +16,3 @@ provider "google" {
 resource "google_compute_network" "hapinet" {
   name = "hapinet"
 }
-
-# See https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance.
-resource "google_compute_instance" "kali" {
-  name         = "kali"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "kali-linux-2023-1-cloud-genericcloud-amd64"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.hapinet.name
-    access_config {
-    }
-  }
-}
