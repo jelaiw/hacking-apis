d5f3b8c74604efb41789e46d15fba3ec746d23b3
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-05-03 01:49:14 +0000

    Move firewall rules.

---
+++ terraform/firewall.tf
@@ -0,0 +1,22 @@
+resource "google_compute_firewall" "hapinet-allow-ssh" {
+  name    = "hapinet-allow-ssh"
+  network = google_compute_network.hapinet.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "hapinet-allow-icmp" {
+  name    = "hapinet-allow-icmp"
+  network = google_compute_network.hapinet.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -17,29 +17,6 @@ resource "google_compute_network" "hapinet" {
   name = "hapinet"
 }
 
-resource "google_compute_firewall" "hapinet-allow-ssh" {
-  name    = "hapinet-allow-ssh"
-  network = google_compute_network.hapinet.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_firewall" "hapinet-allow-icmp" {
-  name    = "hapinet-allow-icmp"
-  network = google_compute_network.hapinet.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
-
 # See https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance.
 resource "google_compute_instance" "kali" {
   name         = "kali"
