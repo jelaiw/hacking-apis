adb660602fba3daad27125d47465b482ce588f17
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-05-09 23:09:23 +0000

    Update Kali VM to e2-standard-2 machine type.

--- terraform/kali.tf
+++ terraform/kali.tf
@@ -1,7 +1,7 @@
 # See https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance.
 resource "google_compute_instance" "kali" {
   name         = "kali"
-  machine_type = "f1-micro"
+  machine_type = "e2-standard-2"
   metadata = {
     "serial-port-enable" = "false"
     "ssh-keys"           = var.ssh-keys
