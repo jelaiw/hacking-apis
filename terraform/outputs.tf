ee9bb59eb4df88bed24eb10412bbad1b9218d637
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-06-02 23:42:42 +0000

    Define kali and vulnweb IP output variables.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "ip" {
+output "kali_ip" {
   value = google_compute_instance.kali.network_interface[0].access_config[0].nat_ip
+}
+
+output "vulnweb_ip" {
+  value = google_compute_instance.vulnweb.network_interface[0].access_config[0].nat_ip
 }
