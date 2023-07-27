fdd31115f7a6535b3f13b24160f93a02d4ea99f1
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-07-27 23:39:36 +0000

    Generate RSA key pair (post create).

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+# Set Terraform input variable for Codespaces VM IP address.
 echo 'export TF_VAR_codespaces_ip=`curl -s https://api.ipify.org`' >> ~/.bashrc
+
+# Install Terraform CLI from Debian package.
 ./.devcontainer/install-terraform.sh
+
+# Generate RSA key pair.
+# Credit to https://stackoverflow.com/a/43235320.
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
