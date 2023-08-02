a715dd62aca96cb86db30d4acac5003479716151
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-08-02 23:41:46 +0000

    Disable ssh host key checking for Ansible.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,3 +8,7 @@ echo 'export TF_VAR_codespaces_ip=`curl -s https://api.ipify.org`' >> ~/.bashrc
 # Generate RSA key pair.
 # Credit to https://stackoverflow.com/a/43235320.
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+
+# Disable ssh host key checking for Ansible.
+# See https://stackoverflow.com/a/30227199.
+echo 'export ANSIBLE_HOST_KEY_CHECKING=False' >> ~/.bashrc
